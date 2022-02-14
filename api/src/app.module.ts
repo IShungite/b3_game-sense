9ff781e765e9947da277ff3c7f2c78470fb9fa2f
Tristan <adrikgw2@gmail.com>
2022-02-14 17:13:35 +0100

    added route shops

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UsersModule } from "./users/users.module";
+import { ShopsModule } from './shops/shops.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot("mongodb+srv://client:qqni3XexM5T3E5XQ@cluster0.yfyha.mongodb.net/Game-sense"),
     UsersModule,
+    ShopsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
