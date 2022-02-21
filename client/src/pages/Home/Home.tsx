635f6edf1275d2d5ea938ca8e5105aafe5db59c0
IShungite <IShungite@gmail.com>
2022-02-21 14:38:05 +0100

    Add MUI

---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import AppBar from "components/AppBar/AppBar";
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <AppBar />
+      <div>Home</div>
+    </div>
+  );
+}
