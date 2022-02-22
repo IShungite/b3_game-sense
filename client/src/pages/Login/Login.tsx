99083a02eb8ba08f032d06ca1fd2459585fedaef
IShungite <ishungite@gmail.com>
2022-02-22 10:27:51 +0100

    Feat: Redirect to home after login

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,12 +1,19 @@
 import { Box, Button, Container, Typography } from "@mui/material";
+import { RouteUrls } from "config";
 import { useAppDispatch, useAppSelector } from "hooks";
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { AuthStatus, login } from "reducers/authSlice";
 
 export default function Login() {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-  const { status, errorMessage } = useAppSelector((state) => state.auth);
+  const { user, status, errorMessage } = useAppSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (user) navigate(RouteUrls.Home);
+  }, [user, navigate]);
 
   const onSubmit = () => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
