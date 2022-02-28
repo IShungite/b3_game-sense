85eeba7dfcc4d25f2c0492d04139d8d6c15ed5a3
Tristan <adrikgw2@gmail.com>
2022-02-28 18:38:49 +0100

    Updated shop schema

--- api/src/shops/schemas/shop.schema.ts
+++ api/src/shops/schemas/shop.schema.ts
@@ -1,7 +1,15 @@
-import { Prop, Schema } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, ObjectId, SchemaTypes } from "mongoose";
+
+export type ShopDocument = Shop & Document;
 
 @Schema()
 export class Shop {
-    @Prop()
+    @Prop({ type: SchemaTypes.ObjectId, auto: true })
+    _id: ObjectId;
+
+    @Prop({ type: String, required: true })
     name: string;
 }
+
+export const ShopSchema = SchemaFactory.createForClass(Shop);
