26c60dfc60e72a9d9e1280ca64b5c7c46bb21e38
Akram Abdoul-Wahab <akram.abdl@outlook.fr>
2022-03-07 18:12:42 +0100

    delete classes entity

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -6,9 +6,10 @@ import { UsersModule } from "./users/users.module";
 import { StatisticsModule } from "./statistics/statistics.module";
 import { GradesModule } from './grades/grades.module';
 import { CharactersModule } from './characters/characters.module';
-import { ClassesModule } from './classes/classes.module';
+
 import { SchoolsModule } from './schools/schools.module';
 import { SubjectsModule } from './subjects/subjects.module';
+import { PromotionsModule } from './promotions/promotions.module';
 @Module({
   imports: [
     MongooseModule.forRoot("mongodb+srv://client:qqni3XexM5T3E5XQ@cluster0.yfyha.mongodb.net/Game-sense"),
@@ -16,9 +17,9 @@ import { SubjectsModule } from './subjects/subjects.module';
     StatisticsModule,
     GradesModule,
     CharactersModule,
-    ClassesModule,
     SchoolsModule,
-    SubjectsModule
+    SubjectsModule,
+    PromotionsModule
   ],
   controllers: [AppController],
   providers: [AppService],
