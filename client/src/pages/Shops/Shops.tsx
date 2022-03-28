2df3c63b1637a2b5f8763f387d49df2ad95a4b00
Tristan <adrikgw2@gmail.com>
2022-03-28 11:57:50 +0200

    started shops page, added product component

--- client/src/pages/Shops/Shops.tsx
+++ client/src/pages/Shops/Shops.tsx
@@ -1,10 +1,11 @@
 import { Container, Typography } from "@mui/material";
+import Product from "components/Shop/Product";
 import React from "react";
 
 export default function Shops() {
   return (
     <Container component="main">
-      <Typography>Shops</Typography>
+      <Product />
     </Container>
   );
 }
