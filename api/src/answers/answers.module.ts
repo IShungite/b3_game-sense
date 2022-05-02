36889a89738b98765f0b483315f8e74d096fa332
Tristan <adrikgw2@gmail.com>
2022-05-02 23:49:17 +0200

    Gain (25*grade) gold when quiz is answered

--- api/src/answers/answers.module.ts
+++ api/src/answers/answers.module.ts
@@ -5,6 +5,7 @@ import { MongooseModule } from "@nestjs/mongoose";
 import { Answer, AnswerSchema } from "./schemas/answer.schema";
 import { Quiz, QuizSchema } from "src/quizzes/schema/quiz.schema";
 import { GradesModule } from "src/grades/grades.module";
+import { CharactersModule } from "src/characters/characters.module";
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { GradesModule } from "src/grades/grades.module";
       { name: Quiz.name, schema: QuizSchema },
     ]),
     GradesModule,
+    CharactersModule,
   ],
   controllers: [AnswersController],
   providers: [AnswersService],
